fb7a59300e60f193737c1cba08fa7ebe99e49f53
JulioEvencio <julioigreja.dev@gmail.com>
2024-07-19 17:42:26 -0300

    refactor

--- Voxel Connection/scenes/scenarios/World/world.gd
+++ Voxel Connection/scenes/scenarios/World/world.gd
@@ -1,10 +1,16 @@
 extends Node2D
 
-var map : Array = []
+@onready var block_scene : PackedScene = preload("res://scenes/blocks/block/block.tscn")
 
 func _ready() -> void:
-	pass
+	for i in range(10):
+		var block : Block = block_scene.instantiate()
+		
+		block.position.x = 128 * i
+		block.position.y = 0
+		
+		add_child(block)
 
-func _process(_delta : float) -> void:
+func _physics_process(_delta : float) -> void:
 	if Input.is_action_just_pressed("escape"):
 		get_tree().quit()
