a4e6c3caca6b5b0d7bd78dd717cde662adc29942
JulioEvencio <julioigreja.dev@gmail.com>
2024-07-19 17:31:46 -0300

    update: player

--- Voxel Connection/scenes/actors/player/player.gd
+++ Voxel Connection/scenes/actors/player/player.gd
@@ -1,28 +1,26 @@
 extends CharacterBody2D
 class_name Player
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED : float = 300.0
+const JUMP_VELOCITY : float = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity : float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _physics_process(delta : float) -> void:
+	move(delta)
 
-func _physics_process(delta):
-	# Add the gravity.
+func move(delta : float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	var direction : float = Input.get_axis("move_left", "move_right")
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
 	move_and_slide()
