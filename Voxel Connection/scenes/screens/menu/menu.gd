aba124b7974dd22be7a7703914936e49290399bd
JulioEvencio <julioigreja.dev@gmail.com>
2024-07-21 03:08:29 -0300

    added: button exit

--- Voxel Connection/scenes/screens/menu/menu.gd
+++ Voxel Connection/scenes/screens/menu/menu.gd
@@ -8,8 +8,14 @@ func start() -> void:
 func credits() -> void:
 	get_tree().change_scene_to_file("res://scenes/screens/credits/credits.tscn")
 
+func exit() -> void:
+	get_tree().quit()
+
 func _on_play_pressed() -> void:
 	Transition.start(func(): start())
 
 func _on_credits_pressed() -> void:
 	Transition.start(func(): credits())
+
+func _on_exit_pressed():
+	Transition.start(func(): exit())
